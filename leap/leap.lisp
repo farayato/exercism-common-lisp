05023449312a9240e5313ec12b1f62012c54e2a0
farayato <farayato0x1f@gmail.com>
2020-06-20 20:27:23 -0300

    few modification suggested by my mentor

--- leap/leap.lisp
+++ leap/leap.lisp
@@ -3,6 +3,18 @@
   (:export #:leap-year-p))
 (in-package #:leap)
 
+#|
+      My old solution
 (defun leap-year-p (year)
   (and (= (mod year 4) 0)
         (or (/= (mod year 100) 0) (= (mod year 400) 0))))
+|#
+
+(defun divisible-by (numerator denominator)
+  (= (mod numerator denominator) 0))
+
+(defun leap-year-p (year)
+  (and (divisible-by year 4)
+       (or (not(divisible-by year 100)) (divisible-by year 400))))
+
+
