0e31e0d79b81209eac0f57c336cb55e7afa71b20
farayato <farayato0x1f@gmail.com>
2020-06-20 00:14:31 -0300

    another one

--- rna-transcription/rna-transcription.lisp
+++ rna-transcription/rna-transcription.lisp
@@ -4,6 +4,14 @@
   (:export #:to-rna))
 (in-package #:rna-transcription)
 
+
+;; G -> C
+;; C -> G
+;; T -> A
+;; A -> U
+
 (defun to-rna (str)
-  "Transcribe a string representing DNA nucleotides to RNA."
-  )
+   "Transcribe a string representing DNA nucleotides to RNA."
+   (map 'string #'(lambda(nucleotide)
+        (getf (list :G #\C :C #\G :T #\A :A #\U) (intern (string nucleotide) "KEYWORD")))
+        str))
